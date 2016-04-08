bc70db6908e705e287e110bf8ebb36fadd42f827
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-04-08 23:36:12 +0200

    Updates to work with latest Core changes

--- src/main/java/src/com/almightyalpaca/discord/bot/plugin/joke/JokePlugin.java
+++ src/main/java/src/com/almightyalpaca/discord/bot/plugin/joke/JokePlugin.java
@@ -4,9 +4,9 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.almightyalpaca.discord.bot.system.command.AbstractCommand;
-import com.almightyalpaca.discord.bot.system.command.annotation.Command;
-import com.almightyalpaca.discord.bot.system.events.CommandEvent;
+import com.almightyalpaca.discord.bot.system.command.Command;
+import com.almightyalpaca.discord.bot.system.command.CommandHandler;
+import com.almightyalpaca.discord.bot.system.events.commands.CommandEvent;
 import com.almightyalpaca.discord.bot.system.exception.PluginLoadingException;
 import com.almightyalpaca.discord.bot.system.exception.PluginUnloadingException;
 import com.almightyalpaca.discord.bot.system.plugins.Plugin;
@@ -20,23 +20,23 @@ import net.dv8tion.jda.MessageBuilder.Formatting;
 import net.dv8tion.jda.entities.User;
 
 public class JokePlugin extends Plugin {
-	class JokeCommand extends AbstractCommand {
+	class JokeCommand extends Command {
 
 		public JokeCommand() {
 			super("joke", "Tell a joke", "joke (name)");
 		}
 
-		@Command(dm = true, guild = true, async = true)
+		@CommandHandler(dm = true, guild = true, async = true)
 		public void onCommand(final CommandEvent event) {
 			this.onCommand(event, (String) null);
 		}
 
-		@Command(dm = true, guild = true, priority = 1, async = true)
+		@CommandHandler(dm = true, guild = true, priority = 1, async = true)
 		public void onCommand(final CommandEvent event, final int id) {
 			this.onCommand(event, id, null, null);
 		}
 
-		@Command(dm = true, guild = true, priority = 1, async = true)
+		@CommandHandler(dm = true, guild = true, priority = 1, async = true)
 		public void onCommand(final CommandEvent event, final int id, final String name) {
 			if (name != null && name.contains(" ")) {
 				final String[] names = name.split(" ", 2);
@@ -46,7 +46,7 @@ public class JokePlugin extends Plugin {
 			}
 		}
 
-		@Command(dm = true, guild = true, priority = 1, async = true)
+		@CommandHandler(dm = true, guild = true, priority = 1, async = true)
 		public void onCommand(final CommandEvent event, final int id, final String firstName, final String lastName) {
 			final MessageBuilder builder = new MessageBuilder();
 
@@ -66,10 +66,6 @@ public class JokePlugin extends Plugin {
 			try {
 				final JSONObject object = new JSONObject(request.asString().getBody());
 
-//				if (object.getString("type") != "success") {
-//					throw new IllegalArgumentException("Lookup failed: " + object.toString(4));
-//				}
-
 				String joke = object.getJSONObject("value").getString("joke");
 
 				joke = StringEscapeUtils.unescapeHtml4(joke);
@@ -82,25 +78,25 @@ public class JokePlugin extends Plugin {
 				builder.appendString("An unexpected error occured!", Formatting.BOLD);
 			}
 
-			event.sendMessage(builder.build());
+			builder.send(event.getChannel());
 		}
 
-		@Command(dm = true, guild = true, priority = 1, async = true)
+		@CommandHandler(dm = true, guild = true, priority = 1, async = true)
 		public void onCommand(final CommandEvent event, final int id, final User user) {
 			this.onCommand(event, id, user.getUsername());
 		}
 
-		@Command(dm = true, guild = true, async = true)
+		@CommandHandler(dm = true, guild = true, async = true)
 		public void onCommand(final CommandEvent event, final String name) {
 			this.onCommand(event, -1, name);
 		}
 
-		@Command(dm = true, guild = true, async = true)
+		@CommandHandler(dm = true, guild = true, async = true)
 		public void onCommand(final CommandEvent event, final String firstName, final String lastName) {
 			this.onCommand(event, -1, firstName, lastName);
 		}
 
-		@Command(dm = true, guild = true, priority = 1, async = true)
+		@CommandHandler(dm = true, guild = true, priority = 1, async = true)
 		public void onCommand(final CommandEvent event, final User user) {
 			this.onCommand(event, -1, user);
 		}
